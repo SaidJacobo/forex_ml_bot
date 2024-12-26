fd1edce1bc062d2356a526d82a8f8e949dca23eb
SaidDev <saidjacobo@outlook.com>
2024-12-26 19:27:01 +0000

    agrego utc+2 en live_trading, ajusto horarios de los bots, agrego

--- live_trading.py
+++ live_trading.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 import yaml
 from backbone.utils.general_purpose import load_function
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -15,7 +15,7 @@ timeframes = {
 }
 
 def siguiente_hora_multiplo(intervalo_horas):
-    now = datetime.now()
+    now = datetime.datetime.now() + datetime.timedelta(hours=2) # <-- FTMO maneja los horarios en GMT + 2 y el servidor esta en utc
     next_hour = (now.hour // intervalo_horas + 1) * intervalo_horas
     if next_hour >= 24:  # Manejar el cambio de día
         next_hour -= 24
