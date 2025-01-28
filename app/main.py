77873b2ccf5f4412325074c0dadbee474afa216f
SaidDev <saidjacobo@outlook.com>
2025-01-28 16:30:42 -0300

    Agrego imagen de fondo en el home

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ if root_dir not in sys.path:
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from app.routers import categories_controller, strategies_controller, backtest_controller
+from app.routers import categories_controller, strategies_controller, backtest_controller, portfolio_controller
 from fastapi.staticfiles import StaticFiles
 
 
@@ -19,13 +19,14 @@ app = FastAPI()
 app.include_router(strategies_controller.router)
 app.include_router(categories_controller.router)
 app.include_router(backtest_controller.router)
+app.include_router(portfolio_controller.router)
 
 templates = Jinja2Templates(directory="./app/templates")
 
 app.mount("/static", StaticFiles(directory="./app/templates/static"), name="static")
-app.mount("/backtest_plots", StaticFiles(directory="./app/templates/static/backtest_plots"), name="backtest_plots")
+# app.mount("/backtest_plots", StaticFiles(directory="./app/templates/static/backtest_plots"), name="backtest_plots")
 
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse("base.html", {"request": request})
+    return templates.TemplateResponse("home.html", {"request": request})
